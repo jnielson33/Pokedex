0c0d35750b57c0263913dfaa0d2904a6e37666cd
jnielson33 <jnielson33@gmail.com>
2016-10-31 15:47:32 -0600

    App completed and functioning. Still a couple things to change to make it better

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Jared Nielson on 10/31/16.
+//  Copyright © 2016 Jared Nielson. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
+
+
+
